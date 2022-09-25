1e3a075013d84aa85d1214c019c4f9467c851e69
O. Riastanjung <oriastan999@gmail.com>
2022-09-25 12:33:23 +0700

    add localStorage as save place

--- js/script.js
+++ js/script.js
@@ -2,6 +2,41 @@ const todos = [];
 
 
 const RENDER_EVENT = "render-todo";
+const SAVED_EVENT = 'saved-todo';
+const STORAGE_KEY = 'TODO_APPS';
+
+
+function isStorageExist() /* boolean */ {
+  if (typeof (Storage) === undefined) {
+    alert('Browser kamu tidak mendukung local storage');
+    return false;
+  }
+  return true;
+}
+
+
+function saveData(){
+  if(isStorageExist()){
+    const parsed = JSON.stringify(todos);
+    localStorage.setItem(STORAGE_KEY, parsed);
+    document.dispatchEvent(new Event(SAVED_EVENT));
+  }
+}
+
+
+function loadDataFromStorage(){
+  const data = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if(data !== null){
+    for(const todo of data){
+      todos.push(todo)
+    }
+  }
+
+  document.dispatchEvent(new Event(RENDER_EVENT))
+}
+
+
 
 function generateId() {
   return +new Date();
@@ -46,6 +81,7 @@ function addTaskToCompleted(todoId){
   todoTarget.isCompleted = true;
 
   document.dispatchEvent(new Event(RENDER_EVENT))
+  saveData();
 }
 
 
@@ -55,8 +91,21 @@ function removeTaskFromCompleted(todoId){
 
   todos.splice(todoTarget,1);
   document.dispatchEvent(new Event(RENDER_EVENT));
+  saveData();
 
 }
+
+function undoTaskFromCompleted(todoId /* HTMLELement */) {
+  const todoTarget = findTodo(todoId);
+
+  if (todoTarget == null) return;
+
+  todoTarget.isCompleted = false;
+  document.dispatchEvent(new Event(RENDER_EVENT));
+
+  saveData()
+}
+
 
 function makeTodo(todoObject) {
   const textTitle = document.createElement("h2");
@@ -100,6 +149,8 @@ function makeTodo(todoObject) {
 
     container.append(checkButton);
   }
+
+  
   return container;
 }
 
@@ -116,6 +167,8 @@ function addTodo() {
   );
   todos.push(todoObject);
   document.dispatchEvent(new Event(RENDER_EVENT));
+
+  saveData();
 }
 
 
@@ -126,6 +179,10 @@ document.addEventListener("DOMContentLoaded", function () {
     e.preventDefault();
     addTodo();
   });
+
+  if (isStorageExist()) {
+    loadDataFromStorage();
+  }
 });
 
 document.addEventListener(RENDER_EVENT, function () {
@@ -153,4 +210,10 @@ document.addEventListener(RENDER_EVENT, function () {
 
     
   // }
+});
+
+
+document.addEventListener(SAVED_EVENT, function () {
+  console.log(localStorage.getItem(STORAGE_KEY));
+  // alert("data disimpan")
 });
